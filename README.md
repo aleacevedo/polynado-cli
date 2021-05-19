73202b33c5111076f664bc2d4b58fb0f213957c9
schoad <schoch.adrian@gmail.com>
2021-05-19 22:59:13 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# polynado-cli
+
+### Mumbai, Mainnet
+1. Add `PRIVATE_KEY` to `.env` file
+2. `./cli.js --help`
+
+Example:
+```bash
+./node cli.js deposit MATIC 0.1 --rpc https://polygon-mumbai.infura.io/v3/{your_APY_KEY}
+```
+> Your note: polynado-matic-0.1-80001-0x4af939d95f65ff85b70e065068ac6601c91178bf9a1f6aef09935bc9045743430e8203f8ff2d948de5e0320d70b4a133bb3e160e0237c7e3ffb798088614
+> Polynado MATIC balance is 0.2
+> Sender account MATIC balance is 0.1941545475
+> Submitting deposit transaction
+> Polynado MATIC balance is 0.3
+> Sender account MATIC balance is 0.0891103175
+> Deposit successful.
+
+```bash
+./cli.js withdraw polynado-matic-0.1-80001-0x4af939d95f65ff85b70e065068ac6601c91178bf9a1f6aef09935bc9045743430e8203f8ff2d948de5e0320d70b4a133bb3e160e0237c7e3ffb798088614 0x67D5A2d8B695f282A7EDAd122f1e4fC2c6998B36 --rpc https://polygon-mumbai.infura.io/v3/{your_APY_KEY}
+```
+
+> Getting current state from polynado contract
+> Generating SNARK proof
+> Proof time: 8070.604ms
+> Submitting withdraw transaction
+> View transaction on maticvigil https://explorer-mumbai.maticvigil.com/tx/0xfb00d0ef91cfcebd288e81926214ba9f58dc3ed14bcf5699c0eda07d8a1f7ea7
+> Done
---
+++ cli.js
@@ -0,0 +1,500 @@
+#!/usr/bin/env node
+// Temporary demo client
+// Works in node.js
+
+require('dotenv').config()
+const fs = require('fs')
+const axios = require('axios')
+const assert = require('assert')
+const snarkjs = require('snarkjs')
+const crypto = require('crypto')
+const circomlib = require('circomlib')
+const bigInt = snarkjs.bigInt
+const merkleTree = require('./lib/MerkleTree')
+const Web3 = require('web3')
+const buildGroth16 = require('websnark/src/groth16')
+const websnarkUtils = require('websnark/src/utils')
+const { toWei, fromWei, toBN, BN } = require('web3-utils')
+const config = require('./config')
+const program = require('commander')
+
+let web3, polynado, circuit, proving_key, groth16, erc20, senderAccount, netId
+let MERKLE_TREE_HEIGHT, MATIC_AMOUNT, TOKEN_AMOUNT, PRIVATE_KEY
+
+/** Whether we are in a browser or node.js */
+const inBrowser = (typeof window !== 'undefined')
+
+/** Generate random number of specified byte length */
+const rbigint = nbytes => snarkjs.bigInt.leBuff2int(crypto.randomBytes(nbytes))
+
+/** Compute pedersen hash */
+const pedersenHash = data => circomlib.babyJub.unpackPoint(circomlib.pedersenHash.hash(data))[0]
+
+/** BigNumber to hex string of specified length */
+function toHex(number, length = 32) {
+  const str = number instanceof Buffer ? number.toString('hex') : bigInt(number).toString(16)
+  return '0x' + str.padStart(length * 2, '0')
+}
+
+/** Display MATIC account balance */
+async function printMATICBalance({ address, name }) {
+  console.log(`${name} MATIC balance is`, web3.utils.fromWei(await web3.eth.getBalance(address)));
+}
+
+/** Display ERC20 account balance */
+/**async function printERC20Balance({ address, name, tokenAddress }) {
+  const erc20ContractJson = require('./build/contracts/ERC20Mock.json')
+  erc20 = tokenAddress ? new web3.eth.Contract(erc20ContractJson.abi, tokenAddress) : erc20
+  console.log(`${name} Token Balance is`, web3.utils.fromWei(await erc20.methods.balanceOf(address).call()))
+}*/
+
+/**
+ * Create deposit object from secret and nullifier
+ */
+function createDeposit({ nullifier, secret }) {
+  const deposit = { nullifier, secret }
+  deposit.preimage = Buffer.concat([deposit.nullifier.leInt2Buff(31), deposit.secret.leInt2Buff(31)])
+  deposit.commitment = pedersenHash(deposit.preimage)
+  deposit.commitmentHex = toHex(deposit.commitment)
+  deposit.nullifierHash = pedersenHash(deposit.nullifier.leInt2Buff(31))
+  deposit.nullifierHex = toHex(deposit.nullifierHash)
+  return deposit
+}
+
+/**
+ * Make a deposit
+ * @param currency Сurrency
+ * @param amount Deposit amount
+ */
+async function deposit({ currency, amount }) {
+  const deposit = createDeposit({ nullifier: rbigint(31), secret: rbigint(31) })
+  const note = toHex(deposit.preimage, 62)
+  const noteString = `polynado-${currency}-${amount}-${netId}-${note}`
+  console.log(`Your note: ${noteString}`)
+  if (currency === 'matic') {
+    await printMATICBalance({ address: polynado._address, name: 'Polynado' })
+    await printMATICBalance({ address: senderAccount, name: 'Sender account' })
+    const value = fromDecimals({ amount, decimals: 18 })
+    console.log('Submitting deposit transaction')
+    await polynado.methods.deposit(toHex(deposit.commitment)).send({ value, from: senderAccount, gas: 2e6 })
+    await printMATICBalance({ address: polynado._address, name: 'Polynado' })
+    await printMATICBalance({ address: senderAccount, name: 'Sender account' })
+    console.log('Deposit successful.')
+  } else { // a token
+    //await printERC20Balance({ address: polynado._address, name: 'Polynado' })
+    //await printERC20Balance({ address: senderAccount, name: 'Sender account' })
+    const decimals = config.deployments[`netId${netId}`][currency].decimals
+    const tokenAmount = fromDecimals({ amount, decimals })
+
+    const allowance = await erc20.methods.allowance(senderAccount, polynado._address).call({ from: senderAccount })
+    console.log('Current allowance is', fromWei(allowance))
+    if (toBN(allowance).lt(toBN(tokenAmount))) {
+      console.log('Approving tokens for deposit')
+      await erc20.methods.approve(polynado._address, tokenAmount).send({ from: senderAccount, gas: 1e6 })
+    }
+
+    console.log('Submitting deposit transaction')
+    await polynado.methods.deposit(toHex(deposit.commitment)).send({ from: senderAccount, gas: 2e6 })
+    //await printERC20Balance({ address: polynado._address, name: 'Polynado' })
+    //await printERC20Balance({ address: senderAccount, name: 'Sender account' })
+  }
+
+  return noteString
+}
+
+/**
+ * Generate merkle tree for a deposit.
+ * Download deposit events from the polynado, reconstructs merkle tree, finds our deposit leaf
+ * in it and generates merkle proof
+ * @param deposit Deposit object
+ */
+async function generateMerkleProof(deposit) {
+  // Get all deposit events from smart contract and assemble merkle tree from them
+  console.log('Getting current state from polynado contract')
+  const events = await polynado.getPastEvents('Deposit', { fromBlock: 13896185, toBlock: 'latest' })
+  const leaves = events
+    .sort((a, b) => a.returnValues.leafIndex - b.returnValues.leafIndex) // Sort events in chronological order
+    .map(e => e.returnValues.commitment)
+  const tree = new merkleTree(MERKLE_TREE_HEIGHT, leaves)
+
+  // Find current commitment in the tree
+  const depositEvent = events.find(e => e.returnValues.commitment === toHex(deposit.commitment))
+  const leafIndex = depositEvent ? depositEvent.returnValues.leafIndex : -1
+
+  // Validate that our data is correct
+  const root = await tree.root()
+  const isValidRoot = await polynado.methods.isKnownRoot(toHex(root)).call()
+  const isSpent = await polynado.methods.isSpent(toHex(deposit.nullifierHash)).call()
+  assert(isValidRoot === true, 'Merkle tree is corrupted')
+  assert(isSpent === false, 'The note is already spent')
+  assert(leafIndex >= 0, 'The deposit is not found in the tree')
+
+  // Compute merkle proof of our commitment
+  return tree.path(leafIndex)
+}
+
+/**
+ * Generate SNARK proof for withdrawal
+ * @param deposit Deposit object
+ * @param recipient Funds recipient
+ * @param relayer Relayer address
+ * @param fee Relayer fee
+ * @param refund Receive ether for exchanged tokens
+ */
+async function generateProof({ deposit, recipient, relayerAddress = 0, fee = 0, refund = 0 }) {
+  // Compute merkle proof of our commitment
+  const { root, path_elements, path_index } = await generateMerkleProof(deposit)
+
+  // Prepare circuit input
+  const input = {
+    // Public snark inputs
+    root: root,
+    nullifierHash: deposit.nullifierHash,
+    recipient: bigInt(recipient),
+    relayer: bigInt(relayerAddress),
+    fee: bigInt(fee),
+    refund: bigInt(refund),
+
+    // Private snark inputs
+    nullifier: deposit.nullifier,
+    secret: deposit.secret,
+    pathElements: path_elements,
+    pathIndices: path_index,
+  }
+
+  console.log('Generating SNARK proof')
+  console.time('Proof time')
+  const proofData = await websnarkUtils.genWitnessAndProve(groth16, input, circuit, proving_key)
+  const { proof } = websnarkUtils.toSolidityInput(proofData)
+  console.timeEnd('Proof time')
+
+  const args = [
+    toHex(input.root),
+    toHex(input.nullifierHash),
+    toHex(input.recipient, 20),
+    toHex(input.relayer, 20),
+    toHex(input.fee),
+    toHex(input.refund)
+  ]
+
+  return { proof, args }
+}
+
+/**
+ * Do an MATIC withdrawal
+ * @param noteString Note to withdraw
+ * @param recipient Recipient address
+ */
+async function withdraw({ deposit, currency, amount, recipient, relayerURL, refund = '0' }) {
+  if (currency === 'matic' && refund !== '0') {
+    throw new Error('The MATIC purchase is supposted to be 0 for MATIC withdrawals')
+  }
+  refund = toWei(refund)
+  if (relayerURL) {
+    if (relayerURL.endsWith('.eth')) {
+      throw new Error('ENS name resolving is not supported. Please provide DNS name of the relayer. See instuctions in README.md')
+    }
+    const relayerStatus = await axios.get(relayerURL + '/status')
+    const { relayerAddress, netId, gasPrices, maticPrices, relayerServiceFee } = relayerStatus.data
+    assert(netId === await web3.eth.net.getId() || netId === '*', 'This relay is for different network')
+    console.log('Relay address: ', relayerAddress)
+
+    const decimals = config.deployments[`netId${netId}`][currency].decimals
+    const fee = calculateFee({ gasPrices, currency, amount, refund, maticPrices, relayerServiceFee, decimals })
+    if (fee.gt(fromDecimals({ amount, decimals }))) {
+      throw new Error('Too high refund')
+    }
+    const { proof, args } = await generateProof({ deposit, recipient, relayerAddress, fee, refund })
+
+    console.log('Sending withdraw transaction through relay')
+    try {
+      const relay = await axios.post(relayerURL + '/relay', { contract: polynado._address, proof, args })
+      if (netId === 137 || netId === 80001) {
+        console.log(`Transaction submitted through the relay. View transaction on maticvigil https://explorer-${getCurrentNetworkName()}.maticvigil.com/tx/${relay.data.txHash}`)
+      } else {
+        console.log(`Transaction submitted through the relay. The transaction hash is ${relay.data.txHash}`)
+      }
+
+      const receipt = await waitForTxReceipt({ txHash: relay.data.txHash })
+      console.log('Transaction mined in block', receipt.blockNumber)
+    } catch (e) {
+      if (e.response) {
+        console.error(e.response.data.error)
+      } else {
+        console.error(e.message)
+      }
+    }
+  } else { // using private key
+    const { proof, args } = await generateProof({ deposit, recipient, refund })
+
+    console.log('Submitting withdraw transaction')
+    await polynado.methods.withdraw(proof, ...args).send({ from: senderAccount, value: refund.toString(), gas: 1e6 })
+      .on('transactionHash', function (txHash) {
+        if (netId === 137 || netId === 80001) {
+          console.log(`View transaction on maticvigil https://explorer-${getCurrentNetworkName()}.maticvigil.com/tx/${txHash}`)
+        } else {
+          console.log(`The transaction hash is ${txHash}`)
+        }
+      }).on('error', function (e) {
+        console.error('on transactionHash error', e.message)
+      })
+  }
+  console.log('Done')
+}
+
+function fromDecimals({ amount, decimals }) {
+  amount = amount.toString()
+  let ether = amount.toString()
+  const base = new BN('10').pow(new BN(decimals))
+  const baseLength = base.toString(10).length - 1 || 1
+
+  const negative = ether.substring(0, 1) === '-'
+  if (negative) {
+    ether = ether.substring(1)
+  }
+
+  if (ether === '.') {
+    throw new Error('[ethjs-unit] while converting number ' + amount + ' to wei, invalid value')
+  }
+
+  // Split it into a whole and fractional part
+  const comps = ether.split('.')
+  if (comps.length > 2) {
+    throw new Error(
+      '[ethjs-unit] while converting number ' + amount + ' to wei,  too many decimal points'
+    )
+  }
+
+  let whole = comps[0]
+  let fraction = comps[1]
+
+  if (!whole) {
+    whole = '0'
+  }
+  if (!fraction) {
+    fraction = '0'
+  }
+  if (fraction.length > baseLength) {
+    throw new Error(
+      '[ethjs-unit] while converting number ' + amount + ' to wei, too many decimal places'
+    )
+  }
+
+  while (fraction.length < baseLength) {
+    fraction += '0'
+  }
+
+  whole = new BN(whole)
+  fraction = new BN(fraction)
+  let wei = whole.mul(base).add(fraction)
+
+  if (negative) {
+    wei = wei.mul(negative)
+  }
+
+  return new BN(wei.toString(10), 10)
+}
+
+function toDecimals(value, decimals, fixed) {
+  const zero = new BN(0)
+  const negative1 = new BN(-1)
+  decimals = decimals || 18
+  fixed = fixed || 7
+
+  value = new BN(value)
+  const negative = value.lt(zero)
+  const base = new BN('10').pow(new BN(decimals))
+  const baseLength = base.toString(10).length - 1 || 1
+
+  if (negative) {
+    value = value.mul(negative1)
+  }
+
+  let fraction = value.mod(base).toString(10)
+  while (fraction.length < baseLength) {
+    fraction = `0${fraction}`
+  }
+  fraction = fraction.match(/^([0-9]*[1-9]|0)(0*)/)[1]
+
+  const whole = value.div(base).toString(10)
+  value = `${whole}${fraction === '0' ? '' : `.${fraction}`}`
+
+  if (negative) {
+    value = `-${value}`
+  }
+
+  if (fixed) {
+    value = value.slice(0, fixed)
+  }
+
+  return value
+}
+
+function getCurrentNetworkName() {
+  switch (netId) {
+  case 137:
+    return 'mainnet'
+  case 80001:
+    return 'mumbai'
+  }
+
+}
+
+function calculateFee({ gasPrices, currency, amount, refund, maticPrices, relayerServiceFee, decimals }) {
+  const decimalsPoint = Math.floor(relayerServiceFee) === Number(relayerServiceFee) ?
+    0 :
+    relayerServiceFee.toString().split('.')[1].length
+  const roundDecimal = 10 ** decimalsPoint
+  const total = toBN(fromDecimals({ amount, decimals }))
+  const feePercent = total.mul(toBN(relayerServiceFee * roundDecimal)).div(toBN(roundDecimal * 100))
+  const expense = toBN(toWei(gasPrices.fast.toString(), 'gwei')).mul(toBN(5e5))
+  let desiredFee
+  switch (currency) {
+  case 'matic': {
+    desiredFee = expense.add(feePercent)
+    break
+  }
+  default: {
+    desiredFee = expense.add(toBN(refund))
+      .mul(toBN(10 ** decimals))
+      .div(toBN(maticPrices[currency]))
+    desiredFee = desiredFee.add(feePercent)
+    break
+  }
+  }
+  return desiredFee
+}
+
+/**
+ * Waits for transaction to be mined
+ * @param txHash Hash of transaction
+ * @param attempts
+ * @param delay
+ */
+function waitForTxReceipt({ txHash, attempts = 60, delay = 1000 }) {
+  return new Promise((resolve, reject) => {
+    const checkForTx = async (txHash, retryAttempt = 0) => {
+      const result = await web3.eth.getTransactionReceipt(txHash)
+      if (!result || !result.blockNumber) {
+        if (retryAttempt <= attempts) {
+          setTimeout(() => checkForTx(txHash, retryAttempt + 1), delay)
+        } else {
+          reject(new Error('tx was not mined'))
+        }
+      } else {
+        resolve(result)
+      }
+    }
+    checkForTx(txHash)
+  })
+}
+
+/**
+ * Parses Polynado.cash note
+ * @param noteString the note
+ */
+function parseNote(noteString) {
+  const noteRegex = /polynado-(?<currency>\w+)-(?<amount>[\d.]+)-(?<netId>\d+)-0x(?<note>[0-9a-fA-F]{124})/g
+  const match = noteRegex.exec(noteString)
+  if (!match) {
+    throw new Error('The note has invalid format')
+  }
+
+  const buf = Buffer.from(match.groups.note, 'hex')
+  const nullifier = bigInt.leBuff2int(buf.slice(0, 31))
+  const secret = bigInt.leBuff2int(buf.slice(31, 62))
+  const deposit = createDeposit({ nullifier, secret })
+  const netId = Number(match.groups.netId)
+
+  return { currency: match.groups.currency, amount: match.groups.amount, netId, deposit }
+}
+
+
+/**
+ * Init web3, contracts, and snark
+ */
+async function init({ rpc, noteNetId, currency, amount }) {
+  let contractJson, erc20ContractJson, erc20polynadoJson, polynadoAddress, tokenAddress
+    // Initialize from local node
+    web3 = new Web3(rpc, null, { transactionConfirmationBlocks: 1 })
+    contractJson = require('./build/contracts/MATICPolynado.json')
+    circuit = require('./build/circuits/withdraw.json')
+    proving_key = fs.readFileSync('build/circuits/withdraw_proving_key.bin').buffer
+    MERKLE_TREE_HEIGHT = process.env.MERKLE_TREE_HEIGHT || 20
+    MATIC_AMOUNT = process.env.MATIC_AMOUNT
+    TOKEN_AMOUNT = process.env.TOKEN_AMOUNT
+    PRIVATE_KEY = process.env.PRIVATE_KEY
+    if (PRIVATE_KEY) {
+      const account = web3.eth.accounts.privateKeyToAccount('0x' + PRIVATE_KEY)
+      web3.eth.accounts.wallet.add('0x' + PRIVATE_KEY)
+      web3.eth.defaultAccount = account.address
+      senderAccount = account.address
+    } else {
+      console.log('Warning! PRIVATE_KEY not found. Please provide PRIVATE_KEY in .env file if you deposit')
+    }
+    //erc20ContractJson = require('./build/contracts/ERC20Mock.json')
+    erc20polynadoJson = require('./build/contracts/ERC20Polynado.json')
+  
+  // groth16 initialises a lot of Promises that will never be resolved, that's why we need to use process.exit to terminate the CLI
+  groth16 = await buildGroth16()
+  netId = await web3.eth.net.getId()
+  if (noteNetId && Number(noteNetId) !== netId) {
+    throw new Error('This note is for a different network. Specify the --rpc option explicitly')
+  }
+  
+  try {
+    polynadoAddress = config.deployments[`netId${netId}`][currency].instanceAddress[amount]
+    if (!polynadoAddress) {
+      throw new Error()
+    }
+    //tokenAddress = config.deployments[`netId${netId}`][currency].tokenAddress
+  } catch (e) {
+    console.error('There is no such polynado instance, check the currency and amount you provide')
+    process.exit(1)
+  }
+  
+  polynado = new web3.eth.Contract(contractJson.abi, polynadoAddress)
+  //erc20 = currency !== 'eth' ? new web3.eth.Contract(erc20ContractJson.abi, tokenAddress) : {}
+}
+
+async function main() {
+    program
+      .option('-r, --rpc <URL>', 'The RPC, CLI should interact with', 'http://localhost:8545')
+      .option('-R, --relayer <URL>', 'Withdraw via relayer')
+    program
+      .command('deposit <currency> <amount>')
+      .description('Submit a deposit of specified currency and amount from default matic account and return the resulting note. The currency is one of (MATIC|DAI|cDAI|USDC|cUSDC|USDT). The amount depends on currency, see config.js file')
+      .action(async (currency, amount) => {
+        currency = currency.toLowerCase()
+        await init({ rpc: program.rpc, currency, amount })
+        await deposit({ currency, amount })
+      })
+    program
+      .command('withdraw <note> <recipient> [MATIC_purchase]')
+      .description('Withdraw a note to a recipient account using relayer or specified private key. You can exchange some of your deposit`s tokens to MATIC during the withdrawal by specifing MATIC_purchase (e.g. 0.01) to pay for gas in future transactions. Also see the --relayer option.')
+      .action(async (noteString, recipient, refund) => {
+        const { currency, amount, netId, deposit } = parseNote(noteString)
+        await init({ rpc: program.rpc, noteNetId: netId, currency, amount })
+        await withdraw({ deposit, currency, amount, recipient, refund, relayerURL: program.relayer })
+      })
+    program
+      .command('balance <address> [token_address]')
+      .description('Check MATIC and ERC20 balance')
+      //.action(async (address, tokenAddress) => {
+      .action(async (address) => {
+        await init({ rpc: program.rpc })
+        await printMATICBalance({ address, name: '' })
+        //if (tokenAddress) {
+          //await printERC20Balance({ address, name: '', tokenAddress })
+        //}
+      })
+    try {
+      await program.parseAsync(process.argv)
+      process.exit(0)
+    } catch (e) {
+      console.log('Error:', e)
+      process.exit(1)
+    }
+}
+
+main()
